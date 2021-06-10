e365a1f6bad3d43864388ea184c9e7c7d42d78b9
Gianluca <gianlucadorelo@gmail.com>
2021-06-10 19:21:45 -0300

    Cleaning

--- api/models/sql/setup_mysql.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS portfi;
-CREATE USER IF NOT EXISTS 'portfi_dev'@'localhost' IDENTIFIED BY 'portfi';
-GRANT ALL PRIVILEGES ON `portfi`.* TO 'portfi_dev'@'localhost';
-GRANT SELECT ON `portfi_schema`.* TO 'portfi_dev'@'localhost';
-FLUSH PRIVILEGES;
